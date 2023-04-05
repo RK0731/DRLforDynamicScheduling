f81468cb1c67f4ee8eea722574ad247a22484fe1
Liu Renke <renkeliu@hmgics.com>
2023-04-05 17:59:47 +0800

    update

---
+++ main_exp.py
@@ -0,0 +1,6 @@
+import logging
+
+import simulator.job as JOB
+import simulator.machine as MACHINE
+
+x = JOB.job()
---
+++ simulator/job.py
@@ -0,0 +1,25 @@
+"""
+This is the job object, carries the information of route, processing time, due date, etc.
+"""
+
+import numpy as np
+
+
+class job:
+    def __init__(self, env, *args, **kwargs):
+        self.env = env
+        # associated attributes
+        self.arrival_t = self.env.now
+        self.j_idx = kwargs['job_index']
+        self.pt = kwargs['processing_time']
+        self.due = kwargs['due_date']
+        # data recording
+        self.processing_record = []
+
+
+    def departure(self):
+        pass
+
+
+    def completion(self):
+        self.tardiness = self.env.now - self.due
